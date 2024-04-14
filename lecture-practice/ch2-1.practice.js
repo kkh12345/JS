ebfaed2e28f6976c53b1ad11238451a3cb8cf934
kkh <rltgjqm9090@gmail.com>
2024-04-14 23:29:43 +0900

    first commit

---
+++ lecture-practice/ch2-1.practice.js
@@ -0,0 +1,39 @@
+// 변수, 자료형, 연산자
+
+let a = 1;
+let b = 2;
+console.log(a, b);
+
+let c = a;
+a = b;
+b = c;
+console.log(a, b);
+
+console.log(20 + 30);
+console.log('Hello' + '' + 2021);
+console.log('20' + '30');
+console.log(1 + 2 * 3);
+console.log((1 + 3) ** 2);
+console.log(1 / 0);
+console.log(6 % 2);
+console.log(7.5 % 2);
+console.log(5 == 5);
+console.log(5 === 5);
+console.log(5 == '5');
+console.log(5 === '5');
+console.log(5 != 5.0);
+console.log(1201 == '120' + 1);
+console.log(5 <= 5.0);
+console.log(5 >= 5.0);
+console.log(true || true);
+console.log(true || false);
+console.log(true && true);
+console.log(true && false);
+console.log(!true);
+console.log(!false);
+console.log(false == 'false');
+console.log(true == 'true');
+console.log(1201 == '120' + 1);
+console.log('abcd' == 'abc' + 'd');
+
+console.log('true' == true);
---
+++ lecture-practice/ch2-2.practice.js
@@ -0,0 +1,32 @@
+// // 배열
+// 관련있는 데이터들을 하나로 묶어서 하나의 변수 아래 저장하는 것
+
+let fruit = ['banana', 'apple', 'grape'];
+console.log(fruit);
+console.log(fruit[1]);
+fruit[0] = 'cherry';
+console.log(fruit[0]);
+
+fruit.pop(); //끝에걸 없애줌
+console.log(fruit);
+fruit.push('fruit');
+console.log(fruit);
+
+//아이템 있는지 없는지 확인
+console.log(fruit.includes('apple'));
+console.log(fruit.includes('grape'));
+
+// 인덱스 번호 확인해주는 함수
+console.log(fruit.indexOf('apple'));
+
+//slice : 배열 아이템을 잘라내는 역할(시작점,끝점-끝점 미포함) 잘라낸 값 반환. 기존 배열은 건드리지 않음
+
+console.log(fruit.slice(2));
+console.log(fruit.slice(1));
+console.log(fruit.slice(0, 2));
+let extra = fruit.slice(0, 2);
+console.log(extra);
+console.log('original fruit', fruit);
+
+//splice : 배열 아이템 잘라내는 역할 (시작점, 개수) 잘라낸 값 반환. 기존 배열 건드림
+console.log(fruit.splice(2, 1));
