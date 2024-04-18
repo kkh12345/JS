5fc0125a32b950a3ead635cc216eadfe94dc1b46
kkh <rltgjqm9090@gmail.com>
2024-04-18 23:31:35 +0900

    commit

--- lecture-practice/ch2-12.practice.js
+++ lecture-practice/ch2-12.practice.js
@@ -1,52 +1,56 @@
-//함수 연습
+//1, 2, 3
 
-//1,2,3
 function greet(name) {
-  console.log('Hi, my name is', name);
+  // console.log('"안녕 내 이름은 ', name + '야"');
   return name;
 }
-const name = greet('jessica');
-console.log(name);
+console.log(greet('kkh'));
 
 //4
 function meetAt(year, month, date) {
   if (date) {
-    return `${year}/${month}/${date}`;
+    return year + '/' + month + '/' + date;
   }
   if (month) {
-    return `${year}년 ${month}월`;
+    return year + '년' + month + '월';
   }
   if (year) {
-    return `${year}년`;
+    return year + '년';
   }
 }
+console.log(meetAt(2022));
+console.log(meetAt(2032, 3));
+console.log(meetAt(1987, 10, 28));
 
-console.log(meetAt(2022, 1, 7));
 //5
 function findSmallestElement(arr) {
-  let result = arr[0];
   if (arr.length === 0) {
     return 0;
-  }
-  for (let i = 1; i < arr.length; i++) {
-    if (result > arr[i]) {
-      result = arr[i];
+  } else {
+    let result = arr[0];
+    for (i = 1; i < arr.length; i++) {
+      result = result > arr[i] ? arr[i] : result;
     }
+    return result;
   }
-  return result;
 }
-
-let smallestNumber = findSmallestElement([100, 200, 3, 0, 2, 1]);
-console.log(smallestNumber);
+console.log(findSmallestElement([100, 200, 3, 1000, 2, 1]));
 
 //6
+// 12300
+// 50000*0
+// 10000*1 2300
+// 5000*0
+// 1000*2 300
+// 500*0
+// 100*3  0
+
 let unit = [50000, 10000, 5000, 1000, 500, 100];
-function changeCalculate(money) {
+function give(money) {
   for (let i = 0; i < unit.length; i++) {
     let num = Math.floor(money / unit[i]);
-    console.log(unit[i] + 'X' + num);
+    console.log(`${unit[i]}X${num}`);
     money = money - unit[i] * num;
   }
 }
-
-changeCalculate(12300);
+give(12300);
