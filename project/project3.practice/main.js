77cb14d1f39cdd8efeb9d1c5cbfae633798ac699
kkh <rltgjqm9090@gmail.com>
2024-04-28 23:16:41 +0900

    4.28

--- project/project3.practice/main.js
+++ project/project3.practice/main.js
@@ -4,9 +4,10 @@ const getLatestNEws = async () => {
   const url = new URL(
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
   ); //URL이라는 인스턴스에서 다양한 필요한 함수,변수를 제공해줌
-  const response = await fetch(url);
-  // console.log('rrr', response);
-  const data = await response.json(); //body에 있는 데이터를 json형식으로 가져와라
+  const response = await fetch(url); //API를 fetch방식으로 부름
+  console.log('rrr', response);
+  const data = await response.json(); // 데이터를 json형식으로 가져와라
+  console.log(data);
   // console.log('ddd', data.articles);
   news = data.articles; //뉴스 데이터 받아옴
   console.log('fff', news);
