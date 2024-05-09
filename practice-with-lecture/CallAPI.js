da336e255f5e84f64d13c6c2d39b840db61b4cd9
kkh <rltgjqm9090@gmail.com>
2024-05-09 23:09:05 +0900

    5/9

--- practice-with-lecture/CallAPI.js
+++
@@ -1,6 +0,0 @@
-const callAPI = async () => {
-  let url = new URI('url주소');
-  let header = new Headers({ 헤더내용 }); //필요한 경우만
-  let response = await fetch(url, { headers: header });
-  let data = await response.json;
-};
