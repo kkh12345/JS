747c6e4b79391a04cc8105b80e7d57c3794c495b
kkh12345 <rltgjqm9090@gmail.com>
2024-05-12 23:34:59 +0900

    5.12

--- LEVEL1/7.js
+++ LEVEL1/7.js
@@ -46,22 +46,55 @@ $('.modal-close-button').on('click', function () {
 //   }
 // }
 
-$('form').on('submit', inputNull);
+// $('form').on('submit', inputNull);
 
-function inputNull(e) {
-  let idValue = $('.modal-id').val();
-  let pwValue = $('.modal-pw').val();
-  if (idValue.length == 0 && pwValue.length == 0) {
-    e.preventDefault(); //폼전송막기
-    alert('id/pw둘다입력하셈');
-  } else if (idValue.length == 0) {
-    e.preventDefault();
-    alert('id를입력하셈');
-  } else if (pwValue.length == 0) {
-    e.preventDefault();
-    alert('pw를입력하셈');
-  } else if (pwValue.length < 6) {
-    alert('6자리이상입력하셈');
-    e.preventDefault();
-  }
+// function inputNull(e) {
+//   let idValue = $('.modal-id').val();
+//   let pwValue = $('.modal-pw').val();
+//   if (idValue.length == 0 && pwValue.length == 0) {
+//     e.preventDefault(); //폼전송막기
+//     alert('id/pw둘다입력하셈');
+//   } else if (idValue.length == 0) {
+//     e.preventDefault();
+//     alert('id를입력하셈');
+//   } else if (pwValue.length == 0) {
+//     e.preventDefault();
+//     alert('pw를입력하셈');
+//   } else if (pwValue.length < 6) {
+//     alert('6자리이상입력하셈');
+//     e.preventDefault();
+//   }
+// }
+
+/* <input> 태그에서 발생하는 이벤트들이 있습니다.
+input이벤트와 change 이벤트인데 <input> 안에 뭔가 입력할 때 발생합니다. 
+ */
+// ▲ <input>에 입력된 값이 변경될 때 input 이벤트가 발생합니다.
+
+// <input>에 뭐 입력해보십시오 그 때마다 콘솔창에 안녕이 출력되는군요.
+// ▲ <input>에 입력된 값이 변경되고 커서를 다른 곳에 찍으면 change 이벤트가 발생합니다.
+
+// <input>에 뭐 입력하고 커서를 다른데 찍어보십시오. 안녕이 출력됩니다.
+
+document
+  .querySelectorAll('.modal-id')[0]
+  .addEventListener('change', function () {
+    console.log('hi');
+  });
+
+if (0) {
+  console.log(1);
 }
+
+// 0
+// ''
+// null
+// undefined
+// NaN
+// 이런 것들은 if문 소괄호 안에서 false랑 같은 역할을 합니다.
+
+// 0제외 모든 숫자
+// '아무문자'
+// []
+// {}
+// 이런 것들은 if문 소괄호 안에서 true랑 같은 역할을 합니다.
