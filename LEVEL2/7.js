64708bda81604812b86e28353c1ae0d39fa68805
kkh12345 <rltgjqm9090@gmail.com>
2024-05-15 23:46:41 +0900

    5.15

---
+++ LEVEL2/7.js
@@ -0,0 +1,14 @@
+let zIndex = 0;
+
+$('.london').on('click', function () {
+  $('.tap-content').html(`<h1>london</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisici cum?</p>`);
+});
+$('.tokyo').on('click', function () {
+  $('.tap-content').html(`<h1>tokyo</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisici cum?</p>`);
+});
+$('.paris').on('click', function () {
+  $('.tap-content').html(`<h1>paris</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisici cum?</p>`);
+});
