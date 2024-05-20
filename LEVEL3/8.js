c29c84accd91947fded47331466d92060883b330
kkh12345 <rltgjqm9090@gmail.com>
2024-05-20 23:10:04 +0900

    5.20

--- LEVEL3/8.js
+++ LEVEL3/8.js
@@ -32,53 +32,149 @@ let products = [
   { id: 1, price: 50000, title: 'Springfield Shirt' },
   { id: 2, price: 60000, title: 'Black Monastery' },
 ];
+let count = 0;
+let 어레이 = [7, 3, 5, 2, 50];
+let 어레이2 = ['a', 'c', 'b'];
+// 어레이.sort(); //문자순정렬
+어레이.sort(function (a, b) {
+  return a - b;
+}); //숫자순정렬 오름차순
+// return b-a; 내림차순
 
-// $('.card-flex').append(템플릿)
-products.forEach(function (data, i) {
-  let 템플릿 = `<div class="card-container">
-<img
-  src="https://via.placeholder.com/600"
-  width="100%"
-  class="card-img"
-/>
-<div class="card-content">
-  <h2 class="card-title">${data['title']}</h2>
-  <p class="card-price">가격 : ${data['price']}</p>
-  <button class="card-button">주문하기</button>
-</div>
-</div>`;
+let 필터 = 어레이.filter(function (a) {
+  return a < 4;
+}); //원본 변형 x 변수에 담아 쓰자
 
-  document
-    .querySelectorAll('.card-flex')[0]
-    .insertAdjacentHTML('beforeend', 템플릿);
+let 새어레이 = 어레이.map(function (a) {
+  return a * 4;
 });
 
-document
-  .querySelector('.card-flex+button')
-  .addEventListener('click', function () {
-    fetch('https://codingapple1.github.io/js/more1.json ')
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        data.forEach((data, i) => {
-          let 템플릿 = `<div class="card-container">
-          <img
-            src="https://via.placeholder.com/600"
-            width="100%"
-            class="card-img"
-          />
-          <div class="card-content">
-            <h2 class="card-title">${data['title']}</h2>
-            <p class="card-price">가격 : ${data['price']}</p>
-            <button class="card-button">주문하기</button>
-          </div>
-          </div>`;
-          document
-            .querySelectorAll('.card-flex')[0]
-            .insertAdjacentHTML('beforeend', 템플릿);
-        });
-      })
-      .catch((error) => {
-        console.log('실패');
-      });
+$('.sort').on('click', function () {
+  console.log('클릭');
+  products.sort(function (a, b) {
+    if (a['title'] < b['title']) {
+      b = 1;
+      a = 0;
+    } else if (a['title'] > b['title']) {
+      a = 1;
+      b = 0;
+    }
+    return b - a;
   });
+  console.log(products);
+  $('.card-flex').html('');
+  repeat(products);
+});
+
+$('.filter').on('click', function () {
+  let 필터 = products.filter(function (a) {
+    return a['price'] <= 60000;
+  });
+  console.log(필터);
+  $('.card-flex').html('');
+  repeat(필터);
+});
+
+$('.price').on('click', function () {
+  console.log(products);
+  products.sort(function (a, b) {
+    return a['price'] - b['price'];
+  });
+  console.log(products);
+  $('.card-flex').html('');
+  repeat(products);
+});
+
+$('.sort2').on('click', function () {
+  products.sort(function (a, b) {
+    if (a['title'] < b['title']) {
+      b = 1;
+      a = 0;
+    } else if (a['title'] > b['title']) {
+      a = 1;
+      b = 0;
+    }
+    return a - b;
+  });
+  console.log(products);
+  $('.card-flex').html('');
+  repeat(products);
+});
+$('.reset').on('click', function () {
+  count = 0;
+  $('.card-flex+button').css('visibility', 'visible');
+  $('.card-flex').html('');
+  products = products.slice(0, 3);
+  console.log(products);
+  repeat(products);
+});
+
+$('input').on('input', function (e) {
+  console.log($(e.currentTarget).val());
+  products.forEach(function (data, i) {
+    if ($(e.currentTarget).val() == data['price']) {
+      console.log(data);
+      $('.card-flex').html('');
+      let 템플릿 = `<div class="card-container">
+    <img
+      src="https://via.placeholder.com/600"
+      width="100%"
+      class="card-img"
+    />
+    <div class="card-content">
+      <h2 class="card-title">${data['title']}</h2>
+      <p class="card-price">가격 : ${data['price']}</p>
+      <button class="card-button">주문하기</button>
+    </div>
+    </div>`;
+      document
+        .querySelectorAll('.card-flex')[0]
+        .insertAdjacentHTML('beforeend', 템플릿);
+    }
+  });
+});
+
+// $('.card-flex').append(템플릿)
+repeat(products);
+
+$('.card-flex+button').on('click', function () {
+  count++;
+  console.log(count);
+
+  $.get(`https://codingapple1.github.io/js/more${count}.json `)
+
+    .then((data) => {
+      console.log(data);
+      data.forEach(function (item, i) {
+        products.push(item);
+      });
+      repeat(data);
+    })
+    .catch((error) => {
+      console.log('실패');
+    });
+  if (count == 2) {
+    count = 0;
+    $('.card-flex+button').css('visibility', 'hidden');
+  }
+});
+
+function repeat(array) {
+  array.forEach((data, i) => {
+    let 템플릿 = `<div class="card-container">
+    <img
+      src="https://via.placeholder.com/600"
+      width="100%"
+      class="card-img"
+    />
+    <div class="card-content">
+      <h2 class="card-title">${data['title']}</h2>
+      <p class="card-price">가격 : ${data['price']}</p>
+      <button class="card-button">주문하기</button>
+    </div>
+    </div>`;
+    document
+      .querySelectorAll('.card-flex')[0]
+      .insertAdjacentHTML('beforeend', 템플릿);
+  });
+}
