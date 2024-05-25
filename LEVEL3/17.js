8d76e9fb0ce60a6c560b19f56e185e2f11eea1e1
kkh12345 <rltgjqm9090@gmail.com>
2024-05-25 18:16:56 +0900

    commit

--- LEVEL3/17.js
+++ LEVEL3/17.js
@@ -1,43 +1,111 @@
+let 마우스다운좌표 = 0;
+let mousedown = false;
+let slideBox = document.querySelectorAll('.slide-box');
+let slideBg = document.querySelectorAll('.slide-bg')[0];
+
 document.querySelectorAll('img').forEach(function (data, i) {
   data.setAttribute('draggable', false);
 });
 
-//slide-box 마우스다운한 상태로 마우스무브하면
-//드래그한 거리만큼 slide-bg움직임
-//마우스업할떄 드래그한거리가 100이상이면 다음사진넘어감
-let 마우스다운좌표 = 0;
-let mousedown = false;
-
-document
-  .querySelectorAll('.slide-box')[0]
-  .addEventListener('mousedown', function (e) {
+slideBox.forEach(function (data, i) {
+  data.addEventListener('mousedown', function (e) {
     mousedown = true;
     마우스다운좌표 = e.clientX;
   });
-document
-  .querySelectorAll('.slide-box')[0]
-  .addEventListener('mousemove', function (e) {
-    if (mousedown === true) {
-      let 움직임 = e.clientX - 마우스다운좌표;
-      document.querySelectorAll(
-        '.slide-bg'
-      )[0].style.transform = `translateX(${움직임}px)`;
-      if (움직임 > 0) {
-        document.querySelectorAll('.slide-bg')[0].style.transform = `none`;
-      }
+});
+
+slideBox[0].addEventListener('mousemove', function (e) {
+  if (mousedown === true) {
+    let 움직임 = e.clientX - 마우스다운좌표;
+    slideBgTransform(움직임);
+    if (움직임 > 0) {
+      slideBgTransform(0);
     }
+  }
+});
+
+slideBox[1].addEventListener('mousemove', function (e) {
+  if (mousedown === true) {
+    let 움직임 = e.clientX - 마우스다운좌표;
+    slideBgTransform(-500 + 움직임);
+  }
+});
+
+slideBox[2].addEventListener('mousemove', function (e) {
+  if (mousedown === true) {
+    let 움직임 = e.clientX - 마우스다운좌표;
+    slideBgTransform(-1000 + 움직임);
+    if (-움직임 > 0) {
+      slideBgTransform(-1000);
+    }
+  }
+});
+
+mouseUpEvent(0, -500, 1, 0);
+mouseUpEvent(1, -1000, 1, 0);
+mouseUpEvent(2, -500, -1, -1000);
+
+slideBox.forEach(function (data, i) {
+  data.addEventListener('touchstart', function (e) {
+    mousedown = true;
+    마우스다운좌표 = e.touches[0].clientX;
   });
-document
-  .querySelectorAll('.slide-box')[0]
-  .addEventListener('mouseup', function (e) {
+});
+
+slideBox[0].addEventListener('touchmove', function (e) {
+  if (mousedown === true) {
+    let 움직임 = e.touches[0].clientX - 마우스다운좌표;
+    slideBgTransform(움직임);
+    if (움직임 > 0) {
+      slideBgTransform(0);
+    }
+  }
+});
+
+slideBox[1].addEventListener('touchmove', function (e) {
+  if (mousedown === true) {
+    let 움직임 = e.touches[0].clientX - 마우스다운좌표;
+    slideBgTransform(-500 + 움직임);
+  }
+});
+
+slideBox[2].addEventListener('touchmove', function (e) {
+  if (mousedown === true) {
+    let 움직임 = e.touches[0].clientX - 마우스다운좌표;
+    slideBgTransform(-1000 + 움직임);
+    if (-움직임 > 0) {
+      slideBgTransform(-1000);
+    }
+  }
+});
+
+touchEndEvent(0, -500, 1, 0);
+touchEndEvent(1, -1000, 1, 0);
+touchEndEvent(2, -500, -1, -1000);
+
+function slideBgTransform(num) {
+  slideBg.style.transform = `translateX(${num}px)`;
+}
+
+function mouseUpEvent(index, ifNum, ifSignNum, elseNum) {
+  slideBox[index].addEventListener('mouseup', function (e) {
     mousedown = false;
-    if (e.clientX - 마우스다운좌표 < -100) {
-      document.querySelectorAll(
-        '.slide-bg'
-      )[0].style.transform = `translateX(-500px)`;
+    if (ifSignNum * (e.clientX - 마우스다운좌표) < -100) {
+      slideBgTransform(ifNum);
     } else {
-      document.querySelectorAll(
-        '.slide-bg'
-      )[0].style.transform = `translateX(0)`;
+      slideBgTransform(elseNum);
     }
   });
+}
+
+function touchEndEvent(index, ifNum, ifSignNum, elseNum) {
+  slideBox[index].addEventListener('touchend', function (e) {
+    mousedown = false;
+
+    if (ifSignNum * (e.changedTouches[0].clientX - 마우스다운좌표) < -100) {
+      slideBgTransform(ifNum);
+    } else {
+      slideBgTransform(elseNum);
+    }
+  });
+}
