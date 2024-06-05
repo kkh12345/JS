37ae87e1e9335493ac58d31ee7e01e2e74a01f34
kkh12345 <rltgjqm9090@gmail.com>
2024-06-05 23:37:44 +0900

    6.5

--- JS-ES6/Part2/1.js
+++ JS-ES6/Part2/1.js
@@ -1,43 +1,67 @@
-function 더하기(a, b = 임시함수()) {
-  console.log(a + b);
-}
-//두번째 파라미터 지정 안했을때 default 10
-//default 파라미터로 숫자연산, 함수도 넣기 가능
-//파라미터 뺴먹었을설떄  설정한 default 파라미터가 넣어짐
-더하기(...[1, 2]);
-더하기(1);
+//apply.call
 
-function 임시함수() {
-  return 15;
-}
+let 어레이 = [10, 20, 30];
 
-//함수의arguments : 함수의 모든 파라미터를 []안에 넣은 변수.
-//array와 유사하지만 array는 아님
-//옛날문법임
-function 함수(임시, a, b, c, d, e, f, g) {
-  console.log(arguments);
-  for (let i = 0; i < arguments.length; i++) {
-    console.log(arguments[i]);
-  }
+function 더하기(a, b, c) {
+  console.log(a + b + c);
 }
-함수(1, 2, 3, 4, 5);
+// 더하기(...어레이);
+더하기.apply(undefined, 어레이);
 
-//만약 임시는 출력하고 싶지 않으면?
-//Rest파라미터
-//... spread operator도 있지만 rest파라미터라는 뜻도 있음
-//rest말고 자유작명가능
-//함수안에 들어온 파라미터를 전부 담은 array에 보관.
-//arguments와 다른점 : 특정할수있음
-function 함수2(a, b, ...rest) {
-  console.log(rest);
-}
-//a,b제외한 뒤에파라미터만 배열에 담아줌.
-//!!!!!rest는 가장 뒤에 써야한다.두번 사용 못함
-함수2(1, 2, 3, 4, 5);
+let person = {
+  인사: function () {
+    console.log(this.name + '안녕');
+  },
+};
+let person2 = {
+  name: '손흥민',
+};
 
-function 함수3(...rest) {
-  rest.forEach((a) => {
-    console.log(a);
-  });
-}
-함수3(4, 403, 402, 3);
+//default파라미터
+// function 더하기(a, b = 임시함수()) {
+//   console.log(a + b);
+// }
+// 더하기(1);
+
+// function 임시함수() {
+//   return 15;
+// }
+//arguments
+// function 함수(a, b, c) {
+//   for (let i = 0; i < arguments.length; i++) {
+//     console.log(arguments[i]);
+//   }
+// }
+// 함수(1, 2, 3);
+
+//rest
+//마지막 파라미터로 넣어야함.2개이상 사용못함
+// function 함수2(...파라미터들) {
+//   파라미터들.forEach((a) => {
+//     console.log(a);
+//   });
+// }
+// 함수2(1, 2, 3, 4, 5, 5);
+
+//연습문제
+//-1-
+// let a = [1, 2, 3];
+// let b = '김밥';
+// let c = [...b, ...a];
+// console.log(c);
+
+//-2-
+// let a = [1, 2, 3];
+// let b = ['you', 'are'];
+// let c = function (a, b) {
+//   console.log([[...a], ...[...b]][1]);
+//   console.log('you');
+// };
+// c(a, b);
+
+//-3,4-
+// let 함수 = function (a = 5, b = a * 2) {
+//   console.log(a + b);
+//   return 10;
+// };
+// 함수(undefined, undefined);
---
+++ react/part1/src/App.js
@@ -0,0 +1,114 @@
+/*eslint-disable*/
+//동적인 ui 만드는 step
+//1.디자인 2.현재상태를 state로 저장 3.state에 따라서 ui가 어떻게 보일지 조건문 등으로 작성
+
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  let [like, changeLike] = useState([0, 0, 0]);
+  let [글제목, 글제목변경] = useState([
+    '남자 코트 추천',
+    '강남 우동 맛집',
+    '파이썬독학',
+  ]);
+  let posts = '강남 우동 맛집';
+  let [modal, setModal] = useState(false);
+  let [color, changeColor] = useState({ background: 'skyBlue' });
+  let [modalTitle, setModalTitle] = useState('');
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = '여자 코트 추천';
+          글제목변경(copy);
+        }}
+      >
+        버튼
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.sort((a, b) => {
+            if (a > b) {
+              return 1;
+            } else if (a < b) {
+              return -1;
+            } else {
+              return 0;
+            }
+          });
+
+          글제목변경(copy);
+        }}
+      >
+        가나다순정렬
+      </button>
+
+      {글제목.map((a, i) => {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                if (modal == false) {
+                  setModal(true);
+                } else {
+                  setModal(false);
+                }
+                setModalTitle(a);
+              }}
+            >
+              {a}
+            </h4>
+            <span
+              onClick={() => {
+                let copy = [...like];
+                copy[i]++;
+                changeLike(copy);
+              }}
+            >
+              👍
+            </span>
+            {like[i]}
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
+
+      {modal == true ? (
+        <Modal
+          modalTitle={modalTitle}
+          color={color}
+          글제목변경={글제목변경}
+          글제목={글제목}
+        ></Modal>
+      ) : null}
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal" style={props.color}>
+      <h4>{props.modalTitle}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button
+        onClick={() => {
+          let copy = [...props.글제목];
+          copy[0] = '여자 코트 추천';
+          props.글제목변경(copy);
+        }}
+      >
+        글수정
+      </button>
+    </div>
+  );
+}
+export default App;
